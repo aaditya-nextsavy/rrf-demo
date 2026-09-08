896da20f8e5f2630f0b6cb16ea5a9d13e241e4eb
Aaditya <aaditya@nextsavy.com>
2026-09-08 19:19:31 +0530

    updated events page, services page

--- assets/scripts/components/header.js
+++ assets/scripts/components/header.js
@@ -1,5 +1,6 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const body = document.body;
+const html = document.documentElement;
 
 if (menuToggle) {
 
@@ -11,15 +12,20 @@ if (menuToggle) {
 
             body.classList.remove("menu-open");
             body.classList.add("menu-closed");
+            html.classList.remove("menu-open");
+            html.classList.add("menu-closed");
 
             setTimeout(() => {
                 body.classList.remove("menu-closed");
+                html.classList.remove("menu-closed");
             }, 700);
 
         } else {
 
             body.classList.remove("menu-closed");
             body.classList.add("menu-open");
+            html.classList.remove("menu-closed");
+            html.classList.add("menu-open");
         }
 
         menuToggle.setAttribute(
